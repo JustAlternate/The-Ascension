1cd6c66213d206b239d0b6f0cbf7915c7ffc9b12
nyoukki <95254760+nyoukki@users.noreply.github.com>
2022-06-20 11:37:13 +0200

    Spirit movement almost done
    We should add inertia to it

--- Script/Spirit.gd
+++ Script/Spirit.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
 
+var velocity = Vector2()
+export var speed = 200
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func get_input():
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	velocity = velocity.normalized() * speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	get_input()
+	move_and_slide(velocity)
+
