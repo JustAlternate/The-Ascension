03040f9910a7084acdadda44d1f9281aa3b2d94a
nyoukki <95254760+nyoukki@users.noreply.github.com>
2022-06-20 15:54:25 +0200

    button

---
+++ Script/Button.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+var pushed = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if pushed:
+		print("J'ai été pushed !")
+
+
+func _on_Button_body_entered(body):
+	if body.is_in_group("tangible"):
+		pushed = true
+
+
+func _on_Button_body_exited(body):
+	if body.is_in_group("tangible"):
+		pushed = false
