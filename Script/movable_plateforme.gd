06f593345166824489dccdf3aa817cb6339e98dd
zazak <zazak.pascal@gmail.com>
2022-06-22 08:34:55 +0200

    changement de movable plateform

--- Script/movable_plateforme.gd
+++ Script/movable_plateforme.gd
@@ -4,9 +4,9 @@ signal path_finished
 
 export var vitesse = 50
 
-var direction = 0 #0=ne bouge pas, 1 = vers l'arrivée, -1 = vers le départ
+export var direction = 0 #0=ne bouge pas, 1 = vers l'arrivée, -1 = vers le départ
 
-var etat = 1 # 1: à l'arrivé, -1: au départ, si la plateforme est en mouvement donne l'emplacement de départ 
+var etat = 1 # 1: à l'arrivée, -1: au départ, si la plateforme est en mouvement donne l'emplacement de départ 
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,12 +16,12 @@ func _ready():
 
 func _physics_process(delta):
 	if direction!=0:
-		$plateforme.global_position += ($"arrivé".global_position - $"départ".global_position)*direction*delta*vitesse/100
-		if $"départ".global_position.distance_to($"arrivé".global_position) < $"départ".global_position.distance_to($plateforme.global_position) and direction==1:
+		$plateforme.global_position += ($"arrivée".global_position - $"départ".global_position)*direction*delta*vitesse/100
+		if $"départ".global_position.distance_to($"arrivée".global_position) < $"départ".global_position.distance_to($plateforme.global_position) and direction==1:
 			emit_signal("path_finished")
-			$plateforme.global_position = $"arrivé".global_position
+			$plateforme.global_position = $"arrivée".global_position
 			direction = 0
-		elif $"départ".global_position.distance_to($"arrivé".global_position) < $"arrivé".global_position.distance_to($plateforme.global_position) and direction==-1:
+		elif $"départ".global_position.distance_to($"arrivée".global_position) < $"arrivée".global_position.distance_to($plateforme.global_position) and direction==-1:
 			emit_signal("path_finished")
 			$plateforme.global_position = $"départ".global_position
 			direction = 0
