25d07baa2c6728bd55825611dbe7617f0b86e8a0
JustAlternate <64951265+JustAlternate@users.noreply.github.com>
2022-06-23 18:09:08 +0200

    prend mon gros push

--- Script/Button.gd
+++ Script/Button.gd
@@ -12,21 +12,20 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(pushed)
-	pass
+	print(pushed)
 
 
 func _on_Button_body_entered(body):
-	if body.is_in_group("tangible") and pushed == 0:
+	if body.is_in_group("tangible") and pushed <= 0:
 		pushed += 1
-		if pushed == 1:
+		if pushed >= 1:
 			$AnimatedSprite.animation = "activation"
 			$AnimatedSprite.frame = 0
 			emit_signal("pushed")
 
 
 func _on_Button_body_exited(body):
-	if body.is_in_group("tangible"):
+	if body.is_in_group("tangible") and pushed > 0:
 		pushed -= 1
 		if pushed == 0:
 			$AnimatedSprite.animation = "desactivation"
