2549e70cce09ae5d4ebe72d111c3ac8d9a933706
nyoukki <95254760+nyoukki@users.noreply.github.com>
2022-06-23 10:16:11 +0200

    box qui tombe

--- Script/Box_alive.gd
+++ Script/Box_alive.gd
@@ -4,7 +4,7 @@ class_name MovableBox
 export var gravity = 500
 
 var velocity = Vector2.ZERO
-var was_on_floor = true
+var was_on_floor = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,12 +14,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	velocity.y += gravity
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity,Vector2(0, -1))
 	velocity = Vector2.ZERO
+	play_fall_sound()
+
+func play_fall_sound():
 	if is_on_floor():
 		if not was_on_floor:
-			print("atterri !")
-			was_on_floor = true
+			$FallSFX.play()
+		was_on_floor = true
 	else:
 		was_on_floor = false
 
