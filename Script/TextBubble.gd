e173a2fcf5deb8593f154ac5237d25e64e3ca108
nyoukki <95254760+nyoukki@users.noreply.github.com>
2022-06-24 11:43:26 +0200

    Text Bubble pour cutscenes

---
+++ Script/TextBubble.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+export var dialogue_npc:String
+signal interacted(instance)
+
+var lebody
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func show_text(text):
+	
+	if $Sprite.visible == false:
+		
+		$Label.show()
+		$Sprite.show()
+		
+		for i in text:
+			yield(get_tree().create_timer(.03),"timeout")
+			$Label.text = $Label.text + i
+		
+		emit_signal("interacted",self)
+
+		yield(get_tree().create_timer(5),"timeout")
+		$Label.text = ""
+		$Sprite.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position = get_parent().get_parent().get_node("body").position
+
