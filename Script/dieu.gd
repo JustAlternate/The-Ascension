be053fda527f28d82454a5df559dce7331ffb92e
zazak <zazak.pascal@gmail.com>
2023-02-06 14:25:49 +0100

    ca commence

---
+++ Script/dieu.gd
@@ -0,0 +1,43 @@
+extends Area2D
+
+#signal revive
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var body_in = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Spirit":
+		body_in = true
+
+
+func _on_Area2D_body_exited(body):
+	if body.name == "Spirit":
+		body_in = false
+
+func _input(event):
+	if event.is_action_pressed("interact"):
+		if body_in:
+			get_tree().call_group("main","revive")
+
+func change_color():
+	if material == load("res://shader_inversion.tres"):
+		material = null
+	else:
+		material = load("res://shader_inversion.tres")
+
+
+func _on_AnimatedSprite_animation_finished():
+	pass # Replace with function body.
